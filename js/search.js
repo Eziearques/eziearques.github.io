893d4e6c48514c8e3d695f3c464d2d8240e6d554
Vaccarezza Vincent <vaccarezza@et.esiea.fr>
2024-12-06 07:19:58 +0100

    search test

---
+++ js/search.js
@@ -0,0 +1,12 @@
+function search() {
+  const elem = getElementById('sr');
+  const words = elem.text.split(' ');
+  const pollutionArray = ["pollution", "plastique", "plastiques", "accumulation", "toxique", "organes", "organe"]
+  for (let i = 0; i < words.length; i++) {
+    if(pollutionArray.indexOf(words[i].toLowerCase()) != -1) {
+      const currentUrl = window.location.host;
+
+      window.location.href = currentUrl+'/page_html/Pollution.html';
+    }
+  }
+}

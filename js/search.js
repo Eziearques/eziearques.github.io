cb3c856550e709798a977d0bcedb4c46925f88f1
Vaccarezza Vincent <vaccarezza@et.esiea.fr>
2024-12-06 07:48:52 +0100

    search test

--- js/search.js
+++ js/search.js
@@ -10,4 +10,31 @@ function search() {
       window.location.href = '/page_html/Pollution.html';
     }
   }
+
+  const planctonArray = ["plancton", "océanique", "oceanique", "poumon", "poumons", "pulmonaire", "phytoplancton"]
+  for (let i = 0; i < words.length; i++) {
+    if(planctonArray.indexOf(words[i].toLowerCase()) != -1) {
+      //const currentUrl = window.location.host;
+      //console.log(currentUrl)
+      window.location.href = '/page_html/Plancton.html';
+    }
+  }
+
+  const mareeArray = ["marée", "maree", "muscle", "atrophie"]
+  for (let i = 0; i < words.length; i++) {
+    if(mareeArray.indexOf(words[i].toLowerCase()) != -1) {
+      //const currentUrl = window.location.host;
+      //console.log(currentUrl)
+      window.location.href = '/page_html/Muscle_Maree.html';
+    }
+  }
+
+  const digestArray = ["digestif", "décomposeur", "decomposeur", "digestion"]
+  for (let i = 0; i < words.length; i++) {
+    if(digestArray.indexOf(words[i].toLowerCase()) != -1) {
+      //const currentUrl = window.location.host;
+      //console.log(currentUrl)
+      window.location.href = '/page_html/Pollution.html';
+    }
+  }
 }
